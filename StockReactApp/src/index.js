5565de5f2990c596e9c9c6af781569e28359e42e
Austin Burton <abur3460@gmail.com>
2020-06-15 22:26:23 -0600

    top level commit

--- StockReactApp/src/index.js
+++ StockReactApp/src/index.js
@@ -7,8 +7,6 @@ import thunk from "redux-thunk";
 import logger from "redux-logger";
 import App from "./App";
 
-const jwt = require("jsonwebtoken");
-
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
@@ -17,15 +15,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) return res.sendStatus(401);
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-}
